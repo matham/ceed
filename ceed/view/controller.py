01c48b57192e85663fe8069bc7e7d390c0261f3e
Matthew Einhorn <moiein2000@gmail.com>
2017-09-15 18:19:08 -0400

    Add workaround for propixx process issues.

--- ceed/view/controller.py
+++ ceed/view/controller.py
@@ -33,7 +33,7 @@ from cplcom.utils import yaml_dumps, yaml_loads
 import ceed
 from ceed.stage import StageFactory, StageDoneException
 from ceed.storage.controller import DataSerializer, CeedData
-from ceed.function import FunctionFactory
+import ceed.view._projector_process
 
 if ceed.has_gui_control or ceed.is_view_inst:
     from kivy.core.window import Window
@@ -798,6 +798,8 @@ class ControllerSideViewControllerBase(ViewControllerBase):
         if PROPixxCTRL is None:
             raise ImportError('Cannot open PROPixx library')
 
+        return ceed.view._projector_process.run_subprocess('pixel', state)
+
         ctrl = PROPixxCTRL()
         if state:
             ctrl.dout.enablePixelMode()
@@ -814,6 +816,8 @@ class ControllerSideViewControllerBase(ViewControllerBase):
         if libdpx is None:
             raise ImportError('Cannot open PROPixx library')
 
+        return ceed.view._projector_process.run_subprocess('led', mode)
+
         libdpx.DPxOpen()
         libdpx.DPxSetPPxLedMask(self.led_modes[mode])
         libdpx.DPxUpdateRegCache()
@@ -826,6 +830,8 @@ class ControllerSideViewControllerBase(ViewControllerBase):
         '''
         if PROPixx is None:
             raise ImportError('Cannot open PROPixx library')
+
+        return ceed.view._projector_process.run_subprocess('video', mode)
 
         dev = PROPixx()
         dev.setDlpSequencerProgram(mode)
