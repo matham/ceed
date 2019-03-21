4ce684baf98f540fc6228d28087c3b060bf8e3de
Matthew Einhorn <moiein2000@gmail.com>
2019-03-21 17:08:20 -0400

    Indicate when mea config changes between/after experiments.

--- ceed/storage/storage_widgets.py
+++ ceed/storage/storage_widgets.py
@@ -1,7 +1,7 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
 from kivy.properties import NumericProperty, BooleanProperty, ObjectProperty, \
-    StringProperty
+    StringProperty, ListProperty
 
 from cplcom.utils import yaml_dumps
 
@@ -25,17 +25,66 @@ class ExperimentLogWidget(BoxLayout):
 
     mea_config = StringProperty('')
 
+    experiment_names = ListProperty([])
+
+    selected_config_str = StringProperty('')
+
+    selected_mea_config_str = StringProperty('')
+
+    _bound_config = None, None, None
+
     def __init__(self, **kwargs):
         super(ExperimentLogWidget, self).__init__(**kwargs)
+
         data_storage = self.data_storage = App.get_running_app().ceed_data
         data_storage.fbind(
             'on_experiment_change', self.experiment_change_callback)
+
+        def set_config(*largs):
+            self.selected_config_str = self.config_str
+        config_uid = self.fbind('config_str', set_config)
+        set_config()
+
+        def set_mea_config(*largs):
+            self.selected_mea_config_str = self.mea_config
+        mea_config_uid = self.fbind('mea_config', set_mea_config)
+        set_mea_config()
+        self._bound_config = self, config_uid, mea_config_uid
+
+    def bind_to_current_config_selection(self, name):
+        obj, uid, mea_uid = self._bound_config
+        obj.unbind_uid('config_str', uid)
+        obj.unbind_uid('mea_config', mea_uid)
+
+        if name == 'app':
+            obj = self
+        else:
+            number = name.split(' ')[1]
+            for widget in self.log_container.children:
+                if isinstance(widget, StageLogWidget):
+                    if widget.experiment_number == number:
+                        obj = widget
+                        break
+            else:
+                assert False
+
+        def set_config(*largs):
+            self.selected_config_str = obj.config_str
+        config_uid = obj.fbind('config_str', set_config)
+        set_config()
+
+        def set_mea_config(*largs):
+            self.selected_mea_config_str = obj.mea_config
+        mea_config_uid = obj.fbind('mea_config', set_mea_config)
+        set_mea_config()
+        self._bound_config = obj, config_uid, mea_config_uid
 
     def experiment_change_callback(self, instance, name, value):
         if name == 'open':
             self.handle_open_file()
         elif name == 'close':
             self.log_text = ''
+            self.experiment_names = []
             for widget in self.log_container.children[:]:
                 self.remove_widget(widget)
         elif name == 'app_log':
@@ -49,6 +98,7 @@ class ExperimentLogWidget(BoxLayout):
                 if isinstance(widget, StageLogWidget):
                     if widget.experiment_number == value:
                         widget.refresh_metadata()
+                        self.mark_experiments_with_changed_config()
                         break
             else:
                 assert False
@@ -56,6 +106,8 @@ class ExperimentLogWidget(BoxLayout):
             data = self.data_storage.get_experiment_metadata(value)
             widget = StageLogWidget(data_storage=self.data_storage, **data)
             self.log_container.add_widget(widget)
+            self.mark_experiments_with_changed_config()
+            self.experiment_names.append('Exp {}'.format(value))
         elif name == 'experiment_notes':
             for widget in self.log_container.children:
                 if isinstance(widget, StageLogWidget):
@@ -67,6 +119,7 @@ class ExperimentLogWidget(BoxLayout):
                 assert False
         elif name == 'app_config':
             self.format_config(self.data_storage.read_config())
+            self.mark_experiments_with_changed_config()
         elif name == 'experiment_mea_settings':
             for widget in self.log_container.children:
                 if isinstance(widget, StageLogWidget):
@@ -76,6 +129,7 @@ class ExperimentLogWidget(BoxLayout):
                         widget.mea_config = \
                             self.data_storage.get_config_mea_matrix_string(
                                 value)
+                        self.mark_experiments_with_changed_config()
                         break
             else:
                 assert False
@@ -101,6 +155,25 @@ class ExperimentLogWidget(BoxLayout):
             widget = cls(data_storage=data_storage, **item)
             self.log_container.add_widget(widget)
 
+        self.experiment_names = [
+            'Exp {}'.format(num) for num in experiment_numbers]
+
+        self.mark_experiments_with_changed_config()
+
+    def mark_experiments_with_changed_config(self):
+        widgets = [
+            w for w in reversed(self.log_container.children)
+            if isinstance(w, StageLogWidget)]
+        if not widgets:
+            return
+
+        for i, widget in enumerate(widgets[:-1]):
+            widget.mea_config_different = \
+                widget.mea_config != widgets[i + 1].mea_config
+
+        widgets[-1].mea_config_different = \
+            widgets[-1].mea_config != self.mea_config
+
     def format_config(self, config):
         s = ''
         for key, value in config.items():
@@ -111,6 +184,25 @@ class ExperimentLogWidget(BoxLayout):
         self.config_str = s
 
         self.mea_config = self.data_storage.get_config_mea_matrix_string()
+
+    def copy_mea_config_to_exp(self, source, target):
+        if source == target:
+            return
+
+        source_block = None
+        if source != 'app':
+            source_block = source.split(' ')[1]
+
+        target_block = target.split(' ')[1]
+
+        s_config = self.data_storage.get_config_mea_matrix_string(source_block)
+        t_config = self.data_storage.get_config_mea_matrix_string(target_block)
+        if s_config == t_config:
+            return
+
+        self.data_storage.set_config_mea_matrix_string(
+            target_block, t_config, s_config)
+        self.mark_experiments_with_changed_config()
 
 
 class LogWidgetBase(object):
@@ -150,6 +242,8 @@ class StageLogWidget(LogWidgetBase, ShowMoreBehavior, BoxLayout):
     config_str = StringProperty('')
 
     mea_config = StringProperty('')
+
+    mea_config_different = BooleanProperty(False)
 
     def refresh_metadata(self):
         data = self.data_storage.get_experiment_metadata(self.experiment_number)
